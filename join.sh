5981ca167b5e63829a13dee6f52fc1f10e77dc4a
tailuge <tailuge@gmail.com>
2022-07-20 14:07:47 +0100

    clean up data

--- join.sh
+++ join.sh
@@ -3,10 +3,12 @@
 FILE=./data/vocab.clean
 CORPUS=./data/corpus.clean
 
-while read line
+while read -r line
 do
     word=$(echo "$line" | cut -f1)
     example=$(grep -m1 "${word}" ${CORPUS})
-    echo -e "$line"'\t'"$example"
+    if [ -n "${example}" ]; then
+	echo -e "$line"'\t'"$example"
+    fi    
 done < ${FILE}
 
--- sanitise-vocab.sh
+++ sanitise-vocab.sh
@@ -3,4 +3,4 @@
 FILE=./data/vocab.raw
 OUTFILE=./data/vocab.clean
 
-grep -P -e '[\p{Han}]' ${FILE} | sort | uniq | tr -s ' ' | cut -c2- | sed 's/ /\t/' |  sed 's/ /\t/' > ${OUTFILE}
+grep -P -e '[\p{Han}]' ${FILE} | sort | uniq | tr -s ' ' | cut -c2- | grep -v '[\p{Han}]* [^ ]* .*[āáǎàēéěèīíǐìōóǒòūúǔùüǘǚǜ]' | grep -v / | sed 's/ /\t/' |  sed 's/ /\t/' > ${OUTFILE}
